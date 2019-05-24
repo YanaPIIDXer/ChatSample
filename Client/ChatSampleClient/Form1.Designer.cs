15272600f8549d28935b7d07f1f366f4a81a206d
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2019-05-24 13:09:10 +0900

    チャットテストクライアント

---
+++ Client/ChatSampleClient/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace ChatSampleClient
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必要なデザイナー変数です。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		/// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows フォーム デザイナーで生成されたコード
+
+		/// <summary>
+		/// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディターで変更しないでください。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.InputField = new System.Windows.Forms.TextBox();
+			this.SendButton = new System.Windows.Forms.Button();
+			this.ChatField = new System.Windows.Forms.RichTextBox();
+			this.SuspendLayout();
+			// 
+			// InputField
+			// 
+			this.InputField.Location = new System.Drawing.Point(30, 404);
+			this.InputField.Name = "InputField";
+			this.InputField.Size = new System.Drawing.Size(634, 22);
+			this.InputField.TabIndex = 0;
+			// 
+			// SendButton
+			// 
+			this.SendButton.Location = new System.Drawing.Point(685, 403);
+			this.SendButton.Name = "SendButton";
+			this.SendButton.Size = new System.Drawing.Size(75, 23);
+			this.SendButton.TabIndex = 1;
+			this.SendButton.Text = "送信";
+			this.SendButton.UseVisualStyleBackColor = true;
+			this.SendButton.Click += new System.EventHandler(this.SendButton_Click);
+			// 
+			// ChatField
+			// 
+			this.ChatField.Location = new System.Drawing.Point(39, 12);
+			this.ChatField.Name = "ChatField";
+			this.ChatField.ReadOnly = true;
+			this.ChatField.Size = new System.Drawing.Size(721, 373);
+			this.ChatField.TabIndex = 2;
+			this.ChatField.Text = "";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.ChatField);
+			this.Controls.Add(this.SendButton);
+			this.Controls.Add(this.InputField);
+			this.Name = "Form1";
+			this.Text = "ChatSampleClient";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox InputField;
+		private System.Windows.Forms.Button SendButton;
+		private System.Windows.Forms.RichTextBox ChatField;
+	}
+}
+
---
+++ Client/ChatSampleClient/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+
+namespace ChatSampleClient
+{
+	public partial class Form1 : Form
+	{
+		/// <summary>
+		/// ソケット
+		/// </summary>
+		Socket Sock = null;
+
+		/// <summary>
+		/// バッファ長
+		/// </summary>
+		private static readonly int BufferSize = 2048;
+
+		/// <summary>
+		/// バッファ
+		/// </summary>
+		private byte[] Buffer = new byte[BufferSize];
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		// フォームが読み込まれた。
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			try
+			{
+				Sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+				Sock.Connect("127.0.0.1", 1234);
+				Sock.BeginReceive(Buffer, 0, BufferSize, SocketFlags.None, new AsyncCallback(ReceiveCallback), Sock);
+			}
+			catch(Exception Ex)
+			{
+				MessageBox.Show("接続に失敗しました。\n" + Ex.Message);
+				Sock = null;
+			}
+		}
+
+		// 送信ボタンが押された。
+		private void SendButton_Click(object sender, EventArgs e)
+		{
+			SendData();
+		}
+		
+		/// <summary>
+		/// データ送信
+		/// </summary>
+		private void SendData()
+		{
+			string SendText = InputField.Text;
+			InputField.Text = "";
+			ChatField.Text += "SEND:" + SendText + Environment.NewLine;
+
+			if (Sock == null) { return; }
+
+			byte[] SendBuffer = Encoding.UTF8.GetBytes(SendText);
+			Sock.Send(SendBuffer);
+		}
+
+		/// <summary>
+		/// 受信コールバック
+		/// </summary>
+		/// <param name="Result">受信結果</param>
+		private void ReceiveCallback(IAsyncResult Result)
+		{
+			var Sk = Result.AsyncState as Socket;
+
+			var RecvSize = -1;
+			try
+			{
+				RecvSize = Sk.EndReceive(Result);
+			}
+			catch { return; }
+
+			if (RecvSize > 0)
+			{
+				ChatField.Text += "RECV:" + Encoding.UTF8.GetString(Buffer) + Environment.NewLine;
+				Sk.BeginReceive(Buffer, 0, BufferSize, SocketFlags.None, ReceiveCallback, Sk);
+			}
+		}
+
+	}
+}
